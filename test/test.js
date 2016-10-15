96b65e130b95ab642a4ecdaafff47ca37d20416c
Felix Zapata <fzberlinches@gmail.com>
2016-10-15 23:34:12 +0200

    refactor(test.js): change how to run the tests

--- test/test.js
+++ test/test.js
@@ -1,36 +1,58 @@
-var grunt;
+'use strict';
 
-grunt = require('grunt');
+var pugI18n = require('../index');
+var path = require('path');
+var fs = require('fs-extra');
+var assert = require('assert');
+require('mocha');
 
-exports.jade_i18n = {
-  translateFile: function(test) {
-    var actual, expected;
-    test.expect(2);
-    expected = grunt.file.read('test/expected/en_US/sample.html');
-    actual = grunt.file.read('.tmp/en_US/sample.html');
-    test.equal(expected, actual, 'should translate the template into english');
-    expected = grunt.file.read('test/expected/es_ES/sample.html');
-    actual = grunt.file.read('.tmp/es_ES/sample.html');
-    test.equal(expected, actual, 'should translate the template into spanish');
-    return test.done();
-  },
-  translateDir: function(test) {
-    var actual, expected;
-    test.expect(2);
-    expected = grunt.file.read('test/expected/template.en_us.html');
-    actual = grunt.file.read('.tmp/template.en_us.html');
-    test.equal(expected, actual, 'should translate the template into english');
-    expected = grunt.file.read('test/expected/template.es_es.html');
-    actual = grunt.file.read('.tmp/template.es_es.html');
-    test.equal(expected, actual, 'should translate the template into spanish');
-    return test.done();
-  },
-  withoutI18n: function(test) {
-    var actual, expected;
-    test.expect(1);
-    expected = grunt.file.read('test/expected/no-i18n.html');
-    actual = grunt.file.read('.tmp/no-i18n.html');
-    test.equal(expected, actual, 'should generate the template without i18n task options');
-    return test.done();
+var fixtures = function (glob) { return path.join(__dirname, './fixtures', glob); }
+
+function fileExists(filePath) {
+  try {
+    return fs.statSync(filePath).isFile();
+  } catch (err) {
+    return false;
   }
-};
+}
+
+describe('gulp-pug-i18n', function () {
+
+	beforeEach(function (done) {
+    var folder = path.join(__dirname, 'temp');
+    fs.remove(folder, done);
+	});
+
+	describe('Translate file', function () {
+
+		it('should translate the template into english', function (done) {
+			var options = {
+				urls: [fixtures('working.html')]
+			};
+			gulp.src(fixtures('working.html'))
+				.pipe(pugI18n(options))
+        .pipe(sassert.end(function () {
+          done();
+        }));
+
+		});
+
+    xit('should translate the template into spanish', function (done) {
+		});
+	});
+
+  xdescribe('Translate directory', function () { 
+    it('should translate the template into english', function (done) { 
+
+    });
+    it('should translate the template into spanish', function (done) { 
+
+    });
+  });
+
+  xdescribe('Without i18n', function() {
+    it('should generate the template without i18n task options', function() {
+
+    });
+  });
+});
